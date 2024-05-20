0a4ae8b75481645cbc6d6ae553009d9b3353f5a6
apecnascimento <apecnascimento@gmail.com>
2024-05-20 17:17:05 -0300

    Add docker utils

---
+++ pyinfra/facts/util/docker.py
@@ -0,0 +1,20 @@
+import json
+import re
+
+
+def convert_key(key):
+    pattern = re.compile(r'(?<!^)(?=[A-Z])')
+    ignored_keys = ["ID", "IPv6", "IPv4"]
+
+    if key in ignored_keys:
+        return key.lower()
+
+    return pattern.sub('_', key).lower()
+
+
+def convert_dict(docker_object):
+    return {convert_key(key): docker_object[key] for key in docker_object}
+
+
+def parse_respose(output):
+    return [convert_dict(json.loads(o)) for o in output]
